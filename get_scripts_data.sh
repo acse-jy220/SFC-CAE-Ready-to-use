f826d3625ed8dbeb069e34734085a38d6b71f12f
acse-jy220 <72440150+acse-jy220@users.noreply.github.com>
2021-06-17 16:51:13 +0800

    Update get_scripts_data.sh

--- get_scripts_data.sh
+++ get_scripts_data.sh
@@ -11,4 +11,7 @@ mkdir -p $DATADIR && cd $DATADIR
 wget https://www.dropbox.com/s/ibpwa5e8xxzyla9/FPC_Re3900_2D_CG_new.zip
 unzip FPC_Re3900_2D_CG_new.zip -d './'
 rm -rf FPC_Re3900_2D_CG_new.zip
+wget https://www.dropbox.com/s/yzh99n64mxpj9db/FPC_Re3900_DG_old.zip
+unzip FPC_Re3900_DG_old.zip -d './'
+rm -rf FPC_Re3900_DG_old.zip
 echo "downloaded the Flow Past Cylinder data and putting it in: " $DATADIR
