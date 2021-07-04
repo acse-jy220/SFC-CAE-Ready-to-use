5025d756bbd74493a91a96d67b33a96e661d3348
acse-jy220 <72440150+acse-jy220@users.noreply.github.com>
2021-07-04 13:35:47 +0800

    Update get_FPC_data.sh

--- get_FPC_data.sh
+++ get_FPC_data.sh
@@ -2,8 +2,12 @@
 # unzip FPC_Re3900_2D_CG_new.zip -d './'
 # rm -rf FPC_Re3900_2D_CG_new.zip
 wget https://www.dropbox.com/s/yzh99n64mxpj9db/FPC_Re3900_DG_old.zip
-wget https://www.dropbox.com/sh/47u4bmbss5ebu9l/AABe7REr__4xIzBtdu4KTFe9a?dl=0
+wget https://www.dropbox.com/s/b0kciap7pgq9cp5/FPC_Re3900_DG_new.zip
 unzip FPC_Re3900_DG_old.zip -d './'
 rm -rf FPC_Re3900_DG_old.zip
-unzip AABe7REr__4xIzBtdu4KTFe9a?dl=0 -d './'
-rm -rf AABe7REr__4xIzBtdu4KTFe9a?dl=0
+unzip FPC_Re3900_DG_new.zip -d './'
+rm -rf FPC_Re3900_DG_new.zip
+
+wget https://www.dropbox.com/sh/aid0yv2685nln51/AADmtVChECW_B85M8O2cDmR0a
+unzip AADmtVChECW_B85M8O2cDmR0a -d './'
+rm -rf AADmtVChECW_B85M8O2cDmR0a
