6ceb535e06a179cd9e608a8f91256ebbd54f9c39
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-07-04 17:26:16 +0800

    Update get_data_slugflow.sh

--- get_data_slugflow.sh
+++ get_data_slugflow.sh
@@ -1,3 +1,3 @@
 wget https://www.dropbox.com/s/fg64qunibm10v3n/slugflow_sixteen.zip
-unzip slugflow_sixteen.zip -d './'
+unzip slugflow_sixteen.zip -d './slugflow_sixteen'
 rm -rf slugflow_sixteen.zip
