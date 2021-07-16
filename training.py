c32e69452a4900f27b3412b6adde98db05471131
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-07-16 13:34:02 +0800

    Update training.py

--- training.py
+++ training.py
@@ -53,7 +53,7 @@ def train(autoencoder, optimizer, criterion, other_metric, dataloader):
       del batch
       del MSE
       del other_MSE
-      print(count)
+    #   print(count)
 
   return train_loss / data_length, train_loss_other/ data_length  # Return MSE
 
@@ -74,7 +74,7 @@ def validate(autoencoder, optimizer, criterion, other_metric, dataloader):
             del x_hat
             del MSE
             del other_MSE
-            print('valid ', count)
+            # print('valid ', count)
 
     return validation_loss / data_length, valid_loss_other / data_length   # Return MSE  
 
