f068f8cd1c3f0bc5ef02499d6d1dd091a290b12f
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-07-21 16:38:57 +0800

    Update training.py

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ import numpy as np
 from util import *
 import util
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def set_seed(seed):
     """
