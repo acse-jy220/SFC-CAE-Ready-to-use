02a1e02e94be595b7d8a8f15a1388bfe1c8d19e9
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-07-30 01:51:57 +0800

    Update training.py

--- training.py
+++ training.py
@@ -242,7 +242,7 @@ def train_model(autoencoder,
   print('test MSE Error: %e' % test_MSE, '| relative MSE Error: %e' % test_MSE_re, '\n Total time used for training: %.2f hour.' % ((total_time_end - total_time_start)/3600)) 
   
   lr_epoch_lists = np.vstack((np.array(lr_change_epoches), np.array(lr_list))).T
-  np.savetxt('lr_changes_at_epoch.txt', lr_epoch_lists)
+  np.savetxt(save_path +'lr_changes_at_epoch.txt', lr_epoch_lists)
 
   MSELoss = np.vstack((np.array(train_MSEs), np.array(valid_MSEs))).T
   reMSELoss = np.vstack((np.array(re_train_MSEs), np.array(re_valid_MSEs))).T
