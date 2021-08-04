1da840f12db2fd2d696fb753783a964f0980ac0f
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-05 00:43:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ for ir in required:
         reqs += ['%s @ %s@master' % (name, ir)]
     else:
         reqs += [ir]
-
-os.system('bash get_scripts.sh')
-
+        
 setup(name='SFC-CAE',
       description="A self-adjusting Space-filling curve (variational) convolutional autoencoder for compressing data on unstructured mesh.",
       url='https://github.com/acse-2020/acse2020-acse9-finalreport-acse-jy220',
