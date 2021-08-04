8dc95498a52c02b34f23652bb94c1e8ee0761507
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-04 18:34:31 +0800

    windows compile Fortran90 with minGW32

--- get_scripts.sh
+++ get_scripts.sh
@@ -2,4 +2,7 @@
 
 # get vtktools and compile fortran library
 wget https://raw.githubusercontent.com/FluidityProject/fluidity/main/python/vtktools.py
+# on linux and mac
 python3 -m numpy.f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new
+# on windows, install mingw32
+# f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new --compiler=mingw32
