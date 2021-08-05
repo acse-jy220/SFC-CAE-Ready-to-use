c69aeb7733728a09bb2b32ea1367994a4aa4ecfa
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-05 21:31:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,12 @@ for ir in required:
     else:
         reqs += [ir]
 
+print(sys.platform)
+
 if sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
    # on linux and mac
    compile_command = 'python -m numpy.f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new'
-elif sys.platform == 'win32':
+elif sys.platform == 'win32' or sys.platform == 'cygwin' or sys.platform == 'msys'
    # on windows
    compile_command = 'f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new --compiler=mingw32'
 else:
