9c0fe41a6c58815909a2b1d7a6411ae87e3e8545
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-05 20:53:38 +0800

    new setup configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
+import sys
 from setuptools import setup, find_packages
+import numpy as np
 from distutils.core import Command
 
 with open('requirements.txt') as f:
@@ -12,7 +14,18 @@ for ir in required:
         reqs += ['%s @ %s@master' % (name, ir)]
     else:
         reqs += [ir]
-        
+
+if sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
+   # on linux and mac
+   compile_command = 'python3 -m numpy.f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new'
+elif sys.platform == 'win32':
+   # on windows
+   compile_command = 'f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new --compiler=mingw32'
+else:
+    raise ValueError('Unknown operating system!')
+
+os.system(compile_command)
+
 setup(name='SFC-CAE',
       description="A self-adjusting Space-filling curve (variational) convolutional autoencoder for compressing data on unstructured mesh.",
       url='https://github.com/acse-2020/acse2020-acse9-finalreport-acse-jy220',
