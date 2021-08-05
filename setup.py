04ac50d7405b193cd67a9bf7b11118a68ab675a9
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-05 21:24:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,15 @@ for ir in required:
     else:
         reqs += [ir]
 
+if sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
+   # on linux and mac
+   compile_command = 'python -m numpy.f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new'
+elif sys.platform == 'win32':
+   # on windows
+   compile_command = 'f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new --compiler=mingw32'
+else:
+    raise ValueError('Unknown operating system!')
+
 setup(name='SFC-CAE',
       description="A self-adjusting Space-filling curve (variational) convolutional autoencoder for compressing data on unstructured mesh.",
       url='https://github.com/acse-2020/acse2020-acse9-finalreport-acse-jy220',
@@ -21,14 +30,5 @@ setup(name='SFC-CAE',
       install_requires=reqs,
       test_suite='tests',
       packages=['sfc_cae'])
-
-if sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
-   # on linux and mac
-   compile_command = 'python3 -m numpy.f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new'
-elif sys.platform == 'win32':
-   # on windows
-   compile_command = 'f2py -c space_filling_decomp_new.f90 -m space_filling_decomp_new --compiler=mingw32'
-else:
-    raise ValueError('Unknown operating system!')
 
 os.system(compile_command)
