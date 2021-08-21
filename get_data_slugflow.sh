72a075545aa9d276eb0496e9eefe93a95a6aab3f
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-08-22 00:33:04 +0800

    Update get_data_slugflow.sh
    Former-commit-id: 024548e9c98adf7fa6d555347a93b1b5d853c58f

--- get_data_slugflow.sh
+++ get_data_slugflow.sh
@@ -1,3 +1,9 @@
 wget https://www.dropbox.com/s/5j3i2jn2ggooagb/Slugflow_80.zip
 unzip Slugflow_80.zip -d './slugflow_80'
 rm -rf Slugflow_80.zip
+
+# use for slugflow training
+wget https://www.dropbox.com/s/52s2efjw9dnxaja/sfcs.pt
+wget https://www.dropbox.com/s/828zfj7nwg6os7c/inv_sfcs.pt
+wget https://www.dropbox.com/s/ajyp3gne1zltl7v/coords.pt
+wget https://www.dropbox.com/s/fggujsqgw8ip21n/cells.npy
