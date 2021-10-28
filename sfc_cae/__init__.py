657236c52e1114bd4cc0709249d07b57854c52c5
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-10-28 13:08:01 +0800

    neighbour md

--- sfc_cae/__init__.py
+++ sfc_cae/__init__.py
@@ -1,6 +1,6 @@
 """A ready-to-use, self-adjusting space-filling-curve (variational) convolutional autoencoder"""
 
-from .utils import *
-from .sfc_cae import *
-from .structured import *
-from .training import *
+# from .utils import *
+# from .sfc_cae import *
+# from .structured import *
+# from .training import *
