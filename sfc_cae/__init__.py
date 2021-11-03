3b400a395aeca90f7e96a2d3d8e5fc3a0dea0e8d
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-11-03 22:49:33 +0800

    debug over, testing........

--- sfc_cae/__init__.py
+++ sfc_cae/__init__.py
@@ -1,6 +1,6 @@
 """A ready-to-use, self-adjusting space-filling-curve (variational) convolutional autoencoder"""
 from .utils import *
 from .sfc_cae import *
-# from .sfc_cae_md import *
+from .sfc_cae_md import *
 from .structured import *
 from .training import *
