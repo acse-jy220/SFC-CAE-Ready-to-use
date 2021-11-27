be3415b1013d4b840cc3cf99f1bcd2bb6b5f7555
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-11-27 13:45:20 +0800

    fix typo

--- sfc_cae/sfc_cae_md.py
+++ sfc_cae/sfc_cae_md.py
@@ -538,7 +538,7 @@ class SFC_CAE_Decoder_md(nn.Module):
         self.convTrans.append([])
         for j in range(1, encoder.size_conv + 1):
            in_channels = encoder.channels[-j]
-           if self.coords_option == 2: in_channels == self.coords_channels[j]
+           if self.coords_option == 2: in_channels += self.coords_channels[-j]
            out_channels = encoder.channels[-j-1]
            if encoder.second_sfc is None:
               self.convTrans[i].append(nn.ConvTranspose1d(in_channels, out_channels, kernel_size=self.kernel_size, stride=self.stride, padding=encoder.padding, output_padding = encoder.output_paddings[j - 1]))
@@ -554,7 +554,7 @@ class SFC_CAE_Decoder_md(nn.Module):
     else:
         for i in range(1, encoder.size_conv + 1):
             in_channels = encoder.channels[-i]
-            if self.coords_option == 2: in_channels == self.coords_channels[i]
+            if self.coords_option == 2: in_channels += self.coords_channels[-i]
             out_channels = encoder.channels[-i-1]
             if encoder.second_sfc is None:
               self.convTrans.append(nn.ConvTranspose1d(in_channels, out_channels, kernel_size=self.kernel_size, stride=self.stride, padding=encoder.padding, output_padding = encoder.output_paddings[i - 1]))
