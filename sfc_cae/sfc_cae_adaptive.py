0bb41a7bad3d6dc73f2b02b4306b57138418b81a
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-12-03 21:59:09 +0800

    mute final activate

--- sfc_cae/sfc_cae_adaptive.py
+++ sfc_cae/sfc_cae_adaptive.py
@@ -674,7 +674,7 @@ class SFC_CAE_Decoder_Adaptive(nn.Module):
     # else: 
     for i in range(len(data_z)): 
         data_z[i] = data_z[i].sum(-1) 
-        data_z[i] = self.activate(data_z[i])
+        # data_z[i] = self.activate(data_z[i])
     return data_z
 
 
