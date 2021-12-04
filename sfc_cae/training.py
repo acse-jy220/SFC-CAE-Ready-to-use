cb7a5a5acff60459d4168b22ddea6c159257b165
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-12-04 22:46:35 +0800

    Update training.py

--- sfc_cae/training.py
+++ sfc_cae/training.py
@@ -277,14 +277,18 @@ def train_adaptive(autoencoder, variational, optimizer, criterion, other_metric,
          for i in range(c_batch_size):
              sfc = batch[1][shuffle_index[i]]
              inv_sfc = batch[2][shuffle_index[i]]
-             print(sfc.shape[-1])
+             print(sfc.shape[-1], batch[1][i].shape[-1])
              if sfc.shape[-1] < batch[1][i].shape[-1]:
                 paras = gen_filling_paras(sfc.shape[-1], batch[1][i].shape[-1])
                 sfcs.append(expand_snapshot_backward_connect(sfc, *paras, False, return_clone = True))
                 inv_sfcs.append(expand_snapshot_backward_connect(inv_sfc, *paras, False, return_clone = True))
              else: 
-                sfcs.append(sfc[..., :batch[1][i].shape[-1]])
-                inv_sfcs.append(inv_sfc[..., :batch[1][i].shape[-1]])
+                sfc = sfc[..., :batch[1][i].shape[-1]]
+                inv_sfc = inv_sfc[..., :batch[1][i].shape[-1]]
+                sfc[sfc >= batch[1][i].shape[-1]] = batch[1][i].shape[-1] - 1
+                inv_sfc[inv_sfc >= batch[1][i].shape[-1]] = batch[1][i].shape[-1] - 1
+                sfcs.append(sfc)
+                inv_sfcs.append(inv_sfc)
       if variational:
         x_hat, KL = autoencoder(data_x, sfcs, inv_sfcs, filling_paras, coords, sfc_shuffle_index)
         for (data_x_i, x_hat_i) in zip(data_x, x_hat): 
