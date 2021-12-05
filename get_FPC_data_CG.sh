ed3dfcf2aaa9ed4faead8d2bf4d84d64adfbe745
acse-jy220 <jin.yu20@imperial.ac.uk>
2021-12-05 17:44:18 +0800

    update dict address

--- get_FPC_data_CG.sh
+++ get_FPC_data_CG.sh
@@ -4,9 +4,5 @@ unzip AADmtVChECW_B85M8O2cDmR0a -d './FPC_Re3900_CG_new/'
 rm -rf AADmtVChECW_B85M8O2cDmR0a
 
 # get two pretrained model dict, one is 2-SFC-CAE, another is 2-SFC-VCAE, both compressed down to 16 latent variables.
-wget https://www.dropbox.com/s/x4earqswako7bf5/Variational_False_Changelr_False_Latent_16_Nearest_neighbouring_False_SFC_nums_2_startlr_0.0001_n_epoches_2000_dict.pth
-wget https://www.dropbox.com/s/m8jceafgej6xzo7/Variational_True_Changelr_False_Latent_16_Nearest_neighbouring_False_SFC_nums_2_startlr_0.0001_n_epoches_2000_dict.pth
-
-# get (inv) sfcs for the above two autoencoders.
-wget https://www.dropbox.com/s/dywgqdrizitc9w8/fpc_cg_sfc_2.pt
-wget https://www.dropbox.com/s/a1batae5f3lb7dy/fpc_cg_invsfc_2.pt
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1SmMWemkN2ykR3Hwa_IoU2ka49P69_G5j' -O 'Variational_False_Changelr_False_Latent_16_Nearest_neighbouring_False_SFC_nums_2_startlr_0.0001_n_epoches_2000_dict.pth'
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Ki3i5wLsdVgx3YupQlLQIZf1A34y5RgN' -O 'Variational_True_Changelr_False_Latent_16_Nearest_neighbouring_False_SFC_nums_2_startlr_0.0001_n_epoches_2000_dict.pth'
