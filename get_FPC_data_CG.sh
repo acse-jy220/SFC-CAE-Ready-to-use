26daa6ed59bfc9f30cce7bfa6cce407d74fd28e4
Jin Yu <72440150+acse-jy220@users.noreply.github.com>
2022-08-21 20:41:19 +0800

    update CG data download link

--- get_FPC_data_CG.sh
+++ get_FPC_data_CG.sh
@@ -1,5 +1,5 @@
 # get the FPC_CG dataset, in '.vtu' format
-gdown https://drive.google.com/uc?id=1BpItXH0Rvwf2NvTBLTIZwr7-LmSyLd9M
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BpItXH0Rvwf2NvTBLTIZwr7-LmSyLd9M' -O 'FPC_Re3900_CG_new.zip'
 unzip FPC_Re3900_CG_new.zip
 # get two pretrained model dict, one is 2-SFC-CAE, another is 2-SFC-VCAE, both compressed down to 16 latent variables.
 wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1SmMWemkN2ykR3Hwa_IoU2ka49P69_G5j' -O 'Variational_False_Changelr_False_Latent_16_Nearest_neighbouring_False_SFC_nums_2_startlr_0.0001_n_epoches_2000_dict.pth'
